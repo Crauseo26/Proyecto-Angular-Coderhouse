3cd918df25945ee86c12a9e46d39424c818f3fb8
Crauseo26 <carlos.rauseo@innovant.us>
2022-04-26 20:03:21 -0300

    [Fix]: Mockapi endpoint

--- src/app/shared/constants/API.services.ts
+++ src/app/shared/constants/API.services.ts
@@ -1,4 +1,4 @@
-export const MOCK_API_ROUTE = "https://6254d50c19bc53e23482c546.mockapi.io/api/";
+export const MOCK_API_ROUTE = "https://626734da78638336421c91a1.mockapi.io/";
 export const GATEWAY_ERROR = 500;
 export const BAD_REQUEST = 401;
 
